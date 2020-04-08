a9fc747183f06285788ad7aaa2902c77db025d02
Leandro Cosas <lecosas@gmail.com>
2020-04-08 08:57:50 -0400

    Initial Commit

---
+++ ShoppingCartList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingCartList
+//
+//  Created by user163948 on 4/8/20.
+//  Copyright © 2020 lecosas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
